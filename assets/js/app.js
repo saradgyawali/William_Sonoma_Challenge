309f16b89f67597da520f0dd4a0000e2186a24e8
Sarad Gyawali <saradgyawali2010@yahoo.com>
2017-05-22 11:00:06 -0700

    Added more jQuery

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,21 @@
 $(document).ready(function() {
 	$("#text").hide();
-	$(".glyphicon-menu-right").click(function(){
+	$(".expand").click(function(){
 		$("#text").slideToggle("slow")
 	})
 
 
-
-
-
-
+   var orgImgSrc = $(".main-img").attr("src");
+   $(".product-small img").mouseover(function(){
+       $(".main-img").attr("src", this.src.replace("small", "large"));
+   });
+    
+   $(".product-small img").mouseout(function(){
+       $(".main-img").attr("src", orgImgSrc);
+   });
+   
+   $(".product-small img").click(function(){
+       orgImgSrc = this.src.replace("small", "large");
+   })
 
 })
